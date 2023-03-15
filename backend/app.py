cfbb13f375d3bd274c2bfc62e4a0636bcd5081f7
Vinti Jain <vintijain1998@gmail.com>
2023-03-14 23:43:20 -0700

    correction in app

--- backend/app.py
+++ backend/app.py
@@ -16,12 +16,10 @@ def home():
 
 @app.route("/scrape/<page_no>",methods=['POST'])
 def scrape_listing(page_no):
-    print(page_no)
     new_scraper = AmazonScraper()
-    print(request.get_json(force=True))
+    #print(request.get_json(force=True))
     data = request.get_json(force=True)
     print(data, page_no)
     reviews = new_scraper.scrapeReviews(data['listing_url'], page_no)
-    reviews = "N"
-    print(reviews)
+    #print(reviews)
     return jsonify(reviews), 200
